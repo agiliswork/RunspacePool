6fd0b1ad151fc27e449e327a218036f58519306d
agiliswork <49206478+agiliswork@users.noreply.github.com>
2023-12-16 19:09:35 +0600

    CreateRunspacePool

---
+++ WaitOne_mutex_git_v2.ps1
@@ -0,0 +1,108 @@
+﻿# Create a log file path with the current date appended
+$logFilePath = Join-Path $env:Temp "Log-$((Get-Date).ToString('ddMMyyyy'))"
+$throttleLimit = 50
+$taskCount = 50
+$timeoutMilliseconds = 5000
+$runspaceTasks = @()
+
+# Define the script block to execute asynchronously
+$scriptBlock = {
+    Param(
+        $index,
+        $logFile,
+        $sleepDuration,
+        $timeout
+    )    
+
+    try {
+        $scriptWorkTime = [math]::Round((Measure-Command { 
+            $mutex = New-Object System.Threading.Mutex($false, "LogMutex")
+            Start-Sleep -Seconds $sleepDuration
+            if ($mutex.WaitOne($timeout) ) {
+                try {
+                    "[$(Get-Date)] | $index ----  Writing time: $sleepDuration Log: $logFile" | Out-File $logFile -Append
+                    Write-Verbose "   $index ----  Writing time: $sleepDuration Log: $logFile" -Verbose
+                } 
+                catch {
+                    Write-Warning $_
+                }
+            } 
+            else {
+                Write-Warning "Timed out acquiring mutex! $index ----  Writing time: $sleepDuration Log: $logFile"
+            }
+            $mutex.ReleaseMutex() | Out-Null
+        }).TotalSeconds, 2)
+    }
+    catch {
+        Write-Warning $_
+        $scriptWorkTime = 0
+    }
+    finally {
+        $mutex.Dispose()
+    }
+    $scriptWorkTime
+}
+
+try {
+    $runspacePool = [runspacefactory]::CreateRunspacePool(
+        1,
+        $throttleLimit, 
+        [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault(), 
+        $host 
+    )
+    $runspacePool.ApartmentState = "MTA"
+    $runspacePool.Open()
+
+    1..$taskCount | ForEach-Object {
+        $sleepDuration = Get-Random (1..10)
+        $powerShellTask = [powershell]::Create() 
+        $powerShellTask.RunspacePool = $runspacePool
+        $powerShellTask.AddScript($scriptBlock).AddArgument($_.ToString()).AddArgument($logFilePath).AddArgument($sleepDuration).AddArgument($timeoutMilliseconds / 2) | Out-Null
+
+        Write-Host "Index: $_  Sleep: $sleepDuration" -ForegroundColor DarkYellow
+
+        $runspaceTasks += [PSCustomObject]@{ 
+            Index   = $_
+            Sleep   = $sleepDuration
+            PowerShell = $powerShellTask
+            AsyncResult = $powerShellTask.BeginInvoke() 
+        }
+    }  
+
+    $stopwatch = [Diagnostics.Stopwatch]::StartNew()
+    $timeRemaining = $timeoutMilliseconds
+
+    $totalWorkTime = [math]::Round((Measure-Command {
+        foreach ($task in $runspaceTasks)        {
+            $timeRemaining = $timeoutMilliseconds - $stopwatch.ElapsedMilliseconds
+            if($timeRemaining -gt 0)
+            {
+                Write-Host $timeRemaining    -ForegroundColor Yellow
+                $task.AsyncResult.AsyncWaitHandle.WaitOne($timeRemaining,$true) | Out-Null
+            }
+        }
+
+        foreach ($task in $runspaceTasks) {  
+            if ($task.AsyncResult.IsCompleted) {
+                $scriptWorkTime = $task.PowerShell.EndInvoke($task.AsyncResult) 
+                Write-Host (Get-Date).ToString() "Done! Index: $($task.Index)  ScriptWorkTime: $scriptWorkTime Sleep: $($task.Sleep)" -ForegroundColor Green
+            }
+            else {
+                Write-Host (Get-Date).ToString() "False! Index: $($task.Index) Sleep: $($task.Sleep)" -ForegroundColor Yellow
+                $task.PowerShell.Stop() | Out-Null
+            }
+            $task.AsyncResult = $null
+            $task.PowerShell.Dispose()
+        }
+    }).TotalSeconds, 2)
+
+    Write-Host "TOTAL WorkTime = $totalWorkTime" 
+} 
+catch {
+    Write-Error "An error occurred: $_"
+}
+finally {
+    $runspaceTasks.Clear()
+    $runspacePool.Close()
+    $runspacePool.Dispose()
+}
